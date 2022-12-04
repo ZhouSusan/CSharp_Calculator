30e5250591527335d74dcde82098fa8d4e35f16b
ZhouSusan <szhou089@gmail.com>
2022-12-03 18:23:10 -0800

    revised Progam.cs and added Calculator.cs to fix Input String was not in a correct format

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string operand)
+        {
+            double result = double.NaN;
+
+            switch (operand) {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-double num1 = 0;
-double num2 = 0;
-
-Console.WriteLine("Calulator\r");
-Console.WriteLine("--------------------------------\n");
-
-
-//User's First number
-Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToDouble(Console.ReadLine());
-
-
-//User's Second Number
-Console.WriteLine("Type another number, and then press Enter.");
-num2 = Convert.ToDouble(Console.ReadLine());
-
-//User's option
-Console.WriteLine("Choose an option from the following list: ");
-Console.WriteLine("\ta - Add");
-Console.WriteLine("\ts - Subtract");
-Console.WriteLine("\tm - Multiply");
-Console.WriteLine("\td - Divide");
-Console.Write("You have selected option: ");
-
-switch (Console.ReadLine())
+﻿class Program
 {
-    case "a":
-        Console.WriteLine($"Result: {num1} + {num2} = " + Math.Round((num1 + num2), 2));
-        break;
-    case "s":
-        Console.WriteLine($"Result : {num1} - {num2} = " + Math.Round((num1 - num2), 2));
-        break;
-    case "m":
-        Console.WriteLine($"Result: {num1} * {num2} = " + Math.Round((num1 * num2), 2));
-        break;
-    case "d":
+    public static void Main(string[] args)
+    {
+        bool endApp = false;
+        Console.WriteLine("Calculator in console: ");
+        Console.WriteLine("-----------------------------------------\n");
 
-        while (num2 == 0)
+        while (!endApp)
         {
-            Console.WriteLine("Enter a non-sero divider: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-        } 
+            //Declare Variables and set to empty
+            string numInput1 = "";
+            string numInput2 = "";
+            double result = 0;
 
-        Console.WriteLine($"Result : {num1} / {num2} = " + Math.Round((num1 / num2), 2));
-        break;
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not a valid input. Please enter an integer value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            Console.Write("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while(!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not a valid number. Please enter an intger value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Substract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your Option: ");
+
+            string operand = Console.ReadLine();
+
+            try {
+                result = Calculator.Calculator.DoOperation(cleanNum1, cleanNum2, operand);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathamtical error.\n");
+                } else
+                {
+                    Console.WriteLine("Yor result: {0:0.##}\n", result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh, no!An expection occurred trying to do the math.\n - Details " + e.Message);
+
+            }
+
+            Console.WriteLine("Press 'n' and Enter to close the app,\n or press any other key and Enter to coninue: ");
+            if (Console.ReadLine() == "n")
+            {
+                endApp = true; 
+            }
+
+           Console.WriteLine("\n");
+        }
+        return;
+    }
 }
-
-Console.WriteLine("Press any key to close the Calculator console app");
-Console.ReadKey();
